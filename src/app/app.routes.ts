d19e2c82ea78423faa1b7971ed413a0787af35ec
DorZevron <dorzevron90@gmail.com>
2025-12-07 17:59:14 +0200

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // { path: 'login', component: LoginComponent },
+    // {
+    //     path: 'search',
+    //     component: SearchComponent,
+    //     canActivate: [authGuard] // דורש JWT
+    // },
+    // {
+    //     path: 'bookmarks',
+    //     component: BookmarksComponent,
+    //     canActivate: [authGuard]
+    // },
+    { path: '', pathMatch: 'full', redirectTo: 'login' },
+    { path: '**', redirectTo: 'login' }
+];
